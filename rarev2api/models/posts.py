5b471c60211d574d795621cdfb065b1178184679
Phantom-Farmer <joeljdahl@gmail.com>
2022-12-13 21:02:38 -0600

    post model built

---
+++ rarev2api/models/posts.py
@@ -0,0 +1,15 @@
+from django.db import models
+from .user import User
+from .category import Category
+
+class Event(models.Model):
+  
+    user_id = models.ForeignKey(UserId, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(CategoryId, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    publication_date = models.DateField(auto_now=True)
+    image_url = models.URLField(max_length=200)
+    content = models.CharField(max_length=1000)
+    approved = models.BooleanField(default=True)
+    
+    
