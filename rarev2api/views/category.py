73444747d684b799142554d78206503c939ee095
Patrick Burns <burns212@gmail.com>
2022-12-17 13:19:49 -0600

    finished exposing category requests, uncommented something in views/post.py

--- rarev2api/views/category.py
+++ rarev2api/views/category.py
@@ -7,13 +7,48 @@ from rest_framework import serializers, status
 
 class CategoryView(ViewSet):
     
-    def list(self, request):
-        
-        categories = Category.objects.all()
-        
-        serializer = CategorySerializer(categories, many=True)
-        
+    def retrieve(self, request, pk):
+        """Handle GET requests for single category
+        """
+        category = Category.objects.get(pk=pk)
+        serializer = CategorySerializer(category)
         return Response(serializer.data)
+    
+    def list(self, request):
+        """Handle get request to list all categories
+        """
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        """Handle POST operations
+        Returns
+            Response -- JSON serialized game instance
+        """
+        category = Category.objects.create(
+            label=request.data["label"]
+        )
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    def update(self, request, pk):
+        """Handle PUT requests for a game
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        category = Category.objects.get(pk=pk)
+        category.label = request.data["label"]
+        category.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        category = Category.objects.get(pk=pk)
+        category.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
     
 class CategorySerializer(serializers.ModelSerializer):
     
